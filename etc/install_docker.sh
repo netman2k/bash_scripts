ca22e2916b7d43dbedb7c3fac83d7593df895f2d
DaeHyung Lee <daehyung.lee@nhnent.com>
2018-05-25 21:54:57 +0900

    docker data directory can be changed

--- etc/install_docker.sh
+++ etc/install_docker.sh
@@ -4,6 +4,8 @@
 #
 # Description:
 #   Installing Docker and composer
+declare -r PROGNAME=$(basename $0)
+declare -r PROGDIR=$(readlink -m $(dirname $0))
 
 function usage {
     cat <<- EOF
@@ -11,11 +13,14 @@ Usage: $PROGNAME [ -h ]
 
 Optional arguments:
     -h: Help
-    -v: enable debug mode
+    -d: Change default docker data directory
+    -s: Disables docker service
+    -v: Enable debug mode
 
 General usage example:
 
   $ $PROGNAME -v 
+  $ $PROGNAME -d /data/docker-data
 
 EOF
 
@@ -38,12 +43,13 @@ function get_pkg_manager() {
 
 
 function parse_arguments {
-    while getopts vdh OPTION
+    while getopts vd:sh OPTION
     do
         case $OPTION in
             h) usage;;
+            d) docker_data_directory=$1;;
             v) set -x;;
-            d) disable_docker_service=true;;
+            s) disable_docker_service=true;;
         esac
     done
 }
@@ -105,10 +111,13 @@ function main {
     echo "Add $USER into the docker group..."
     sudo usermod -aG docker $USER
 
+    # Change the default data location
+    if [ -n $docker_data_directory ];then 
+      echo -e "{\n\t\"data-root\": \"${docker_data_directory}\"\n}" > /etc/docker/daemon.json
+    fi
+
     sudo service docker start
     [ $disable_docker_service ] || sudo service enable docker
 }
 
 main "$@"
-
-
