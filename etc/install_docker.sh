9950680d87585af404add6cf8c618faef7e85b9d
DaeHyung Lee <daehyung.lee@nhnent.com>
2018-05-25 19:53:11 +0900

    fixed an errata

--- etc/install_docker.sh
+++ etc/install_docker.sh
@@ -38,13 +38,12 @@ function get_pkg_manager() {
 
 
 function parse_arguments {
-    while getopts vdh --long no-docker-compose OPTION
+    while getopts vdh OPTION
     do
         case $OPTION in
             h) usage;;
             v) set -x;;
             d) disable_docker_service=true;;
-            no-docker-compose) no_docker_compose=true;;
         esac
     done
 }
@@ -52,48 +51,48 @@ function parse_arguments {
 function install_docker(){
   local pkg_mgr=${1}
 
-	echo
-	echo "Installing Docker..."
-	echo
+  echo
+  echo "Installing Docker..."
+  echo
 
   if [[ $pkg_mgr =~ (apt[-get]?) ]];then
-		sudo apt-get remove docker docker-engine docker.io
-		sudo apt-get update
-		sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-			 $(lsb_release -cs) stable"
-		sudo apt-get update
-		sudo apt-get install docker-ce
+    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce
     
 
   elif [ $pkg_mgr = "yum" ];then
-		sudo yum remove docker docker-client docker-client-latest docker-common \
+    sudo yum remove docker docker-client docker-client-latest docker-common \
          docker-latest docker-latest-logrotate docker-logrotate docker-selinux \
-         docker-engine-selinux docker-engine
-		sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-		sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-		sudo yum install docker-ce
+         docker-engine-selinux docker-engine -y
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce
 
   fi
 }
 
 
 function install_docker_compose(){
- 	echo
-	echo "Installing Docker compose..."
-	echo
+  echo
+  echo "Installing Docker compose..."
+  echo
 
- 	sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` \
- 	-o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` \
+  -o /usr/local/bin/docker-compose
   sudo curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose \
-	-o /etc/bash_completion.d/docker-compose
+  -o /etc/bash_completion.d/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
 function main {
 
-    [ ${#@} -eq 0 ] && { usage; exit 1; }
+    #[ ${#@} -eq 0 ] && { usage; exit 1; }
 
     parse_arguments "$@"
     
@@ -102,11 +101,11 @@ function main {
     install_docker $pkg_mgr
     [ $no_docker_compose ] || install_docker_compose
 
-		echo
-		echo "Add $USER into the docker group..."
+    echo
+    echo "Add $USER into the docker group..."
     sudo usermod -aG docker $USER
 
-		sudo service docker start
+    sudo service docker start
     [ $disable_docker_service ] || sudo service enable docker
 }
 
